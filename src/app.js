55973dac462e7395e44e30c6e7394810c71168f8
Nayana <nayanank.23@gmail.com>
2025-03-23 20:58:11 +0530

    "playing with routes"

--- src/app.js
+++ src/app.js
@@ -1,8 +1,53 @@
 const express = require ("express");
 const app = new express();
-app.use((req,res)=>{
-res.send("hello from nayana's express server")
-})
+
+//this will match all the HTTP method  API calls to /test
+    app.use("/test", (req, res) => {
+        res.send("test Server started ")
+    })
+    // this will call only  handle GET call to /user
+    app.get("/user",(req,res)=>{
+     res.send({firstName: "Nayana",lastName:"Kiran"})
+    })
+    app.post("/user",(req,res)=>{
+        //saving data to db
+        res.send("data successfully saved")
+       })
+    app.delete("/user",(req,res)=>{
+        res.send("deleted user sucessfully")
+    })
+    app.patch("/user",(req,res)=>{
+        res.send("patched user sucessfully");
+    })
+    app.put("/user",(req,res)=>{
+        res.send("updated user data");
+    })
+    //dynamic routes
+    app.get("/user/:userId/:password",(req,res)=>{
+        console.log(req.params);
+        res.send({firstName:"Nayana",lastName:"Kumar"})
+    })
+    //playing with * ,? ,/regex/
+    app.get("/user/*fly",(req,res)=>{
+        console.log(req.query);
+        res.send({firstName:"Nayana",lastName:"Kumar"})
+    })
+    //b is optional abc and ac also runs
+    app.get("/user/ab?c",(req,res)=>{
+        console.log(req.query);
+        res.send({firstName:"Nayana",lastName:"Kumar"})
+    })
+    //can add anything after ab last should be c
+    app.get("/user/ab+c",(req,res)=>{
+        console.log(req.query);
+        res.send({firstName:"Nayana",lastName:"Kumar"})
+    })
+    //can bc is optional abcd , ad runs
+    app.get("/user/a(bc)?d",(req,res)=>{
+        console.log(req.query);
+        res.send({firstName:"Nayana",lastName:"Kumar"})
+    })
+  
 app.listen(3000,()=>{
     console.log("server is listening sucessfully on port 3000")
 });
