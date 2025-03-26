50e61145b474d8cf3a7e89533e8009510ddf528c
Nayana <nayanank.23@gmail.com>
2025-03-26 23:42:19 +0530

    error handling

--- keypoints.md
+++ keypoints.md
@@ -1,1 +1,4 @@
-//route handler-middlewares, which handles req and sends res - request handler in between are middlewares
+# route handler-middlewares, which handles req and sends res - request handler in between are middlewares
+# when we're adding err in app.use it should be the first order is err> req >res>next
+# error handling using app.us("/", err, req,res,next) always keep it in the end
+# best way of handling error is using try and catch but we can always keep app.use("/", err, req,res,send) at the end so anything breaks it handles it
--- src/app.js
+++ src/app.js
@@ -1,62 +1,39 @@
 const express = require ("express");
 const app = new express();
 
-//this will match all the HTTP method  API calls to /test
+const { adminAuth,userAuth } = require ("./middlewares/auth");
 
-//route handler-middlewares, which handles req and sends res - request handler in between are middlewares
-    app.use("/test", (req, res,next) => {
-        //res.send("test Server started ");
-        next();
-    },[(req,res,next)=>{
-        console.log("response 2");
-        // res.send("server's second response")
-        next();
-    },(req,res,next)=>{
-        res.send("third server response");
-    }])
-    // this will call only  handle GET call to /user
-    app.get("/user",(req,res)=>{
-     res.send({firstName: "Nayana",lastName:"Kiran"})
-    })
-    app.post("/user",(req,res)=>{
-        //saving data to db
-        res.send("data successfully saved")
-       })
-    app.delete("/user",(req,res)=>{
-        res.send("deleted user sucessfully")
-    })
-    app.patch("/user",(req,res)=>{
-        res.send("patched user sucessfully");
-    })
-    app.put("/user",(req,res)=>{
-        res.send("updated user data");
-    })
-    //dynamic routes
-    app.get("/user/:userId/:password",(req,res)=>{
-        console.log(req.params);
-        res.send({firstName:"Nayana",lastName:"Kumar"})
-    })
-    //playing with * ,? ,/regex/
-    app.get("/user/*fly",(req,res)=>{
-        console.log(req.query);
-        res.send({firstName:"Nayana",lastName:"Kumar"})
-    })
-    //b is optional abc and ac also runs
-    app.get("/user/ab?c",(req,res)=>{
-        console.log(req.query);
-        res.send({firstName:"Nayana",lastName:"Kumar"})
-    })
-    //can add anything after ab last should be c
-    app.get("/user/ab+c",(req,res)=>{
-        console.log(req.query);
-        res.send({firstName:"Nayana",lastName:"Kumar"})
-    })
-    //can bc is optional abcd , ad runs
-    app.get("/user/a(bc)?d",(req,res)=>{
-        console.log(req.query);
-        res.send({firstName:"Nayana",lastName:"Kumar"})
-    })
-  
+ app.use("/admin",adminAuth);
+
+
+ app.post("/user/login",(req,res,next) =>{
+    res.send(" user login")
+ })
+ app.use("/user/data",userAuth, (req,res,next)=>{
+    res.send("user details");
+ })
+
+ app.use("/user",userAuth, (req,res,next)=>{
+    res.send("user authenticated");
+ })
+
+app.get("/admin/getAlldata", (req,res)=>{
+    res.send("all data sent")
+})
+app.get("/admin/deleteUser",(req,res,next)=>{
+    try{
+    throw new Error ("gdhadpsoxjhdbqdcx")
+    res.send("user deleted")}
+    catch (err) {
+        res.status(500).send("something went wrong contact support ")
+    }
+})
+//eror handling
+// app.use("/",(err,req,res,next) =>{
+//     if(err){
+//         res.status(500).send(err.message)
+//     }
+// })
 app.listen(3000,()=>{
     console.log("server is listening sucessfully on port 3000")
 });
