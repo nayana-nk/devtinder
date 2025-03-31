83a8043199783a65e98b76609088535e3bb35390
Nayana <nayanank.23@gmail.com>
2025-03-31 22:42:46 +0530

    crud operation

--- src/config/database.js
+++ src/config/database.js
@@ -8,9 +8,28 @@ async function main() {
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
+    
     const collection = db.collection('user');
 
-  
+    const insertResult = await collection.insertOne({
+      firstName: "Nayana",
+      lastName: "Kiran",
+      location: "Bengaluru"
+    });
+
+    console.log('Inserted document =>', insertResult);
+    //fetching the data
+    const findResult = await collection.find({}).toArray();
+    //console.log('Found documents =>', findResult);
+    const filteredDocs = await collection.find({ firstName:"Nayana" }).toArray();
+    // console.log('Found documents filtered by { firstName: Nayana } =>', filteredDocs);
+     const updateResult = await collection.updateOne(  { firstName: "Nayana" },
+      { $set: { age: 25 } });
+// console.log('Updated documents =>', updateResult);
+ //delete 
+ //const deleteResult = await collection.deleteOne({ b: 1 }); // Ensure correct type
+ //console.log('Delete result =>', deleteResult);
+
     return 'done.';
   }
   main()
