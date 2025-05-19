467def230ec596fc0019d7da3d72826123041f03
Nayana <nayanank.23@gmail.com>
2025-05-19 20:46:15 +0530

    updated

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const  mongoose = require("mongoose");
 
 
 const connectDB = async ()=>{
-    await  mongoose.connect("mongodb+srv://nayanank23:Nayaki23@nayananode.cr9nk.mongodb.net/Hello")
+    await  mongoose.connect("DATABASE_PASSWORD")
 }
 
 module.exports =connectDB ;	
