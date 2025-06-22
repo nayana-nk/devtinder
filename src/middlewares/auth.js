c9caa2c6ae69d378ff32809f8eb14432b0aa866c
Nayana <nayanank.23@gmail.com>
2025-06-22 20:35:34 +0530

    added .env files

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,8 @@ const userAuth = async (req, res, next) => {
     const { token } = req.cookies; // Ensure you have cookie-parser enabled in your server
     if (!token) throw new Error("Token not provided");
 
-    const decoded = jwt.verify(token, "DEV_TINDER@23");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+   
     const user = await User.findById(decoded._id);
     if (!user) throw new Error("User not found");
 
